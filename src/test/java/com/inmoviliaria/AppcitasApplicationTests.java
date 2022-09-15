3840230b7e6531cec4127622d850f467c1a47d42
edwar moran <edwar.moran.12@gmail.com>
2022-09-14 20:47:59 -0500

    proyecto inicial

---
+++ src/test/java/com/inmoviliaria/AppcitasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inmoviliaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppcitasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
